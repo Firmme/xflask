d331b4e1d119948aee992701c6bf06e5fe3e10b8
Firmme <614269474@qq.com>
2017-08-16 23:54:47 +0800

    sql

--- hello.py
+++ hello.py
@@ -1,32 +1,64 @@
 # -- coding:utf-8 --
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+from flask_script import Manager
+import os
 
 app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SECRET_KEY'] = 'xiao'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite ')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+db = SQLAlchemy(app)
+manager = Manager(app)
 
-app.config['SECRET_KEY'] = 'xiao'
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+    users = db.relationship('User', backref='role')
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column(db.String(64), unique=True, index=True)
+
+    def __repr__(self):
+        return '<User %r>' % self.user_name
+
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
 
 class NameForm(FlaskForm):
-    name = StringField(u'你的名字是?',validators=[DataRequired()])
+    name = StringField(u'你的名字是?', validators=[DataRequired()])
     submit = SubmitField(u'提交')
 
 
 @app.route('/', methods=['get', 'post'])
 def index():
-    name = None
     form = NameForm()
     if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
-    return render_template('index.html', form=form, name=name,
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash(u'看起来你换了一个名字呢!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'),
                            current_time=datetime.utcnow())
 
 
@@ -46,4 +78,4 @@ def internal_server_error(e):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
