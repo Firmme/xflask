a0ed1940696ebe0621e6cc1dfd717fd588ddc1bc
Firmme <614269474@qq.com>
2017-08-18 08:55:28 +0800

    bug

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 # -- coding:utf-8 --
-from flask import Flask, render_template, session, redirect, url_for, flash
+from flask import Flask, render_template, session, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
@@ -9,6 +9,7 @@ from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand
+from flask_mail import Message, Mail
 import os
 
 app = Flask(__name__)
@@ -17,12 +18,34 @@ app.config['SECRET_KEY'] = 'xiao'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite ')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['DEBUG'] = True
+app.config['MAIL_SERVER'] = 'SMTP.QQ.COM'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('USER_PASSWORD')
+app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = '[FLASKY]'
+app.config['FLASKY_SENDER'] = 'Flasky Admin<firmme@qq.com>'
+app.config['FLASKY_ADMIN'] = os.environ.get('FLASKY_ADMIN')
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 db = SQLAlchemy(app)
 manager = Manager(app)
 migrate = Migrate(app, db)
+mail = Mail(app)
+
+
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Role=Role)
+
+
+def send_email(to, subject, template, **kwargs):
+    msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject,
+                  sender=app.config['FLASKY_SENDER'], recipients=[to])
+    msg.body = render_template(template + '.txt', **kwargs)
+    msg.html = render_template(template + '.html', **kwargs)
+    mail.send(msg)
+
 
 manager.add_command('db', MigrateCommand)
 manager.add_command("shell", Shell(make_context=make_shell_context))
@@ -55,10 +78,6 @@ class NameForm(FlaskForm):
     submit = SubmitField(u'提交')
 
 
-def make_shell_context():
-    return dict(app=app, db=db, User=User, Role=Role)
-
-
 @app.route('/', methods=['get', 'post'])
 def index():
     form = NameForm()
@@ -68,9 +87,12 @@ def index():
             user = User(username=form.name.data)
             db.session.add(user)
             session['known'] = False
+            if app.config['FLASKY_ADMIN']:
+                print user
+                send_email(app.config['FLASKY_ADMIN'], 'New User',
+                           'mail/new_user', user=user)
         else:
             session['known'] = True
-        print user
         session['name'] = form.name.data
         form.name.data = ''
         return redirect(url_for('index'))
